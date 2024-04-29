4a9f0bbdd964681aa87a98b137be18427afb492c
devmanoharrr <devmanoharrr@gmail.com>
2024-04-29 17:52:10 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,6 @@ import heroImg03 from "../assets/images/hero-img03.jpg";
 import icon01 from "../assets/images/icon01.jpg";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
-import featureImg from "../assets/images/feature-img.png";
-import videoIcon from "../assets/images/video-icon.png";
-import avatarIcon from "../assets/images/avatar-icon.png";
 import faqImg from "../assets/images/faq-img.jpg";
 
 import { Link } from "react-router-dom";
